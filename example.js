51e56ba81d439b702c5f0476b6ae32bcaf79c6cd
dev0x10 <dev.yauri@gmail.com>
2013-12-28 00:33:27 +0800

    implement all gcm parameters

--- example.js
+++ example.js
@@ -9,11 +9,31 @@ var gcm = new NGcm.Gcm("YOUR SERVER KEY from Google cloud console");
 //Send process will be repeated by maxAttempt (if not set, default is 5)
 gcm.maxAttempt = 2;
 
+//below are parameters for GCM message
+//Ref: http://developer.android.com/google/gcm/server.html
+
 //the android device gcm id(s) must be an array
-gcm.setReceiver(["RECEIVER ID"]);
+gcm.setReceiver(["RECEIVERS_ID"]);
 
 //message should be key and value pair
 gcm.addMessage("key1", "value1");
+
+
+//----------------OPTIONAL
+
+//parameter is boolean value
+//gcm.testMode(true);
+
+//parameter is integer (seconds)
+//gcm.ttl(120);
+
+//sparameter is string
+//gcm.collapseKey("Update");
+
+//parameter is boolean value
+//gcm.delayWhileIdle(true);
+
+//-------------------------
 
 
 //if send success, it returns JSON object success response
